46d18a5a20a832f8c833b24caa8a3419172a318b
Otakawai <quentin.detre4254@gmail.com>
2023-01-19 11:57:05 +0100

    Format date et heure changé

--- app/src/main/java/com/redgunner/droidsoft/adapter/PostListAdapter.kt
+++ app/src/main/java/com/redgunner/droidsoft/adapter/PostListAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +16,7 @@ import com.redgunner.droidsoft.R
 import com.redgunner.droidsoft.models.post.Post
 import kotlinx.android.synthetic.main.post_view_holder_layout.view.*
 import java.text.SimpleDateFormat
+import kotlin.time.hours
 
 class PostListAdapter(val postClick: (postId: Int) -> Unit) :
     PagingDataAdapter<Post, PostListAdapter.PostViewHolder>(POST_COMPARATOR) {
@@ -65,10 +67,60 @@ class PostListAdapter(val postClick: (postId: Int) -> Unit) :
 
             category.text = post._embedded.wp_Term[0][0].name
 
+            val sdFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(post.date).toString()
+            Log.d("TAG",sdFormat)
 
-            time.text = SimpleDateFormat("yyyy-MM-dd").parse(post.date).toString()
+            val heure = sdFormat[11] + "" + sdFormat[12] + "h" + sdFormat[14] + "" + sdFormat[15]
+
+            val date =sdFormat[8] + "" + sdFormat[9] + "/" + getMonth(sdFormat[4] +""+ sdFormat[5] + sdFormat[6]) + "/" + sdFormat[30] + sdFormat[31] + sdFormat[32] + sdFormat[33]
+
+            val heureDate = heure + " " + date
+
+            time.text = heureDate
 
             // Log.
+        }
+
+        fun getMonth(str : String):String {
+            var month = "00"
+            if (str == "Jan") {
+                month = "01"
+            }
+            else if(str == "Feb") {
+                month = "02"
+            }
+            else if(str == "Mar") {
+                month = "03"
+            }
+            else if(str == "Apr") {
+                month = "04"
+            }
+            else if(str == "May") {
+                month = "05"
+            }
+            else if(str == "Jun") {
+                month = "06"
+            }
+            else if(str == "Jul") {
+                month = "07"
+            }
+            else if(str == "Aug") {
+                month = "08"
+            }
+            else if(str == "Sep") {
+                month = "09"
+            }
+            else if(str == "Oct") {
+                month = "10"
+            }
+            else if(str == "Nov") {
+                month = "11"
+            }
+            else {
+                month = "12"
+            }
+
+            return month
         }
 
 
