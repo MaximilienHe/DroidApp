835ec756865823ec101d6b3dc7465c926e339edb
Otakawai <quentin.detre4254@gmail.com>
2023-01-26 15:21:42 +0100

    Creation SearchFragment.kt

---
+++ app/src/main/java/com/redgunner/droidsoft/view/fragment/SearchFragment.kt
@@ -0,0 +1,9 @@
+package com.redgunner.droidsoft.view.fragment
+
+import androidx.fragment.app.Fragment
+import com.redgunner.droidsoft.R
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+
+}
